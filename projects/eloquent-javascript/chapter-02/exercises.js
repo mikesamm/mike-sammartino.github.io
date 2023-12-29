7c6cb2ed49b1b8ac194db7f6a3c52c5615b52406
Mike Sammartino <sammartino.mike@gmail.com>
2023-12-29 01:47:44 +0000

    finished eloquent ch2 exercises

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -5,31 +5,92 @@
 
 function triangles(rows) { // NEEDS A PARAMETER
   // rows will be upper bound for the loop
-  let hashes = '#';
-  
-  // loop for rows amount of lines
-  for (let i = 1; i <= rows; i++) {
-    // loop for rows amount of hashes
-    for (let j = 0; j <= rows; j++) {
-      // modify hashes with addition operators
-      hashes += '#';
-    }
+  let hashes = '';
+
+  // loop for rows amount of hashes
+  for (let j = 1; j <= rows; j++) {
+    // modify hashes with addition operator
+    hashes += '#';
+    console.log(hashes);
   }
+
 }
+
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
-
+// write a program that prints out numbers from 1 to 100,
+// for numbers divisible by 3, print 'fizz' instead
+// for numbers divisible by 5, print 'buzz' instead
+// for numbers divisble by 3 AND 5, print 'fizzbuzz'
 function fizzBuzz() {
-  
+  // create a loop that will execute 100 times
+  for (let i = 1; i < 16; i++) {
+    // most special case first
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log('fizzbuzz');
+    } else if (i % 5 === 0) {
+      console.log('buzz');
+    } else if (i % 3 === 0) {
+      console.log('fizz');
+    } else {
+      console.log(i);
+    }
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
+// write a program that prints a string, where in the console it looks like a chessboard
+function drawChessboard(size) {  // NEEDS A PARAMETER
+  // storage variable for string output
+  let chessBoard = '';
 
-function drawChessboard() {  // NEEDS A PARAMETER
+  /*
+  for odd rows:
+    it should add a ' ' on odd iterations
+    it should add a '#' on even iterations
+    it should add a '\n' at the end of the row
+  for even rows:
+    it should add a '#' on odd iterations
+    it should add a ' ' on even iterations
+    it should add a '\n' at the end of the row
+  */
+  
+  // loop for each row
+  for (let i = 1; i <= size; i++) {
+    // if row is even
+    if (i % 2 === 0) {
+      let charSlot = 1;
 
+      while (charSlot <= size) {
+        if (charSlot % 2 === 0) { // if iteration is even
+          chessBoard += ' ';
+        } else {  // if iteration is odd
+          chessBoard += '#';
+        }
+        charSlot++;
+      }
+      // add a new line char
+      chessBoard += '\n';
+    } else {  // if row is odd
+      let charSlot = 1;
+
+      while (charSlot <= size) {
+        if (charSlot % 2 === 0) {  // if iteration is even
+          chessBoard += '#';
+        } else {  // if iteration is odd
+          chessBoard += ' ';
+        }
+        charSlot++;
+      }
+      // add a new line char
+      chessBoard += '\n';
+    }
+  }
+  // print the entire string
+  console.log(chessBoard);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
