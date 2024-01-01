a82537a7e23a86369f227e14023ccd0d4d7cdba6
Mike Sammartino <sammartino.mike@gmail.com>
2024-01-01 20:07:01 +0000

    structure of doc added

---
+++ studies/operators.js
@@ -0,0 +1,17 @@
+/**
+ * Operators
+ * 
+ * 1. Assignment: = += -= /= *=
+ * 2. Arithmetic: + - / * %
+ * 3. Comparison: === !== == != < > =< >=
+ * 4. Logical: && ||
+ * 5. Unary: typeof ...
+ * 6. Ternary / Conditional: (a ? b : c)
+ */
+
+// Assignment
+// Arithmetic
+// Comparison
+// Logical
+// Unary
+// Ternary
