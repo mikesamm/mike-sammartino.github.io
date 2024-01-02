983a8a4fae4200e6594b2bd71135e0bbb41209fb
Mike Sammartino <sammartino.mike@gmail.com>
2024-01-02 22:36:25 +0000

    rough draft on operators completed

--- studies/operators.js
+++ studies/operators.js
@@ -1,6 +1,8 @@
 /**
  * Operators
  * 
+ * Operators are used to perform operations on data and in JavaScript, there are several types of them. They range from acting on one operand (unary) to three operands (ternary), with the majority acting on two operand (binary). Depending on the operator, operands will need to be certain data types to be useful, whether they are literal values or variables.
+ * 
  * 1. Assignment: = += -= /= *=
  * 2. Arithmetic: + - / * %
  * 3. Comparison: === !== == != < > =< >=
@@ -10,8 +12,87 @@
  */
 
 // Assignment
+
+/**
+ * These operators assign values to variables.
+
+| Operator | Action                          | Example |
+| -------- | ------------------------------- | ------- |
+| =        | assignment                      |         |
+| +=       | assignment-addition combo       |         |
+| -=       | assignment-subtraction combo    |         |
+| \*=      | assignment-multiplication combo |         |
+| /=       | assignment-division combo       |         |
+| %=       | assignment-modulo combo         |         |
+
+ */
+
 // Arithmetic
+
+/**
+ * These operators perform arithmetic operations on data.
+| Operator | Action             | Example |
+| -------- | ------------------ | ------- |
+| +        | addition           |         |
+| -        | subtraction        |         |
+| \*       | multiplication     |         |
+| /        | division           |         |
+| %        | modulo (remainder) |         |
+| ++       | increment by one   |         |
+| --       | decrement by one   |         |
+| **       | exponentiation     |         |
+ */
+
 // Comparison
+
+/**
+ * These operators resolve to a boolean, comparing two pieces of data.
+| Operator | Action                   | Example |
+| -------- | ------------------------ | ------- |
+| ===      | strictly equals          |         |
+| \==      | loosely equals           |         |
+| >=       | greater than or equal to |         |
+| <=       | less than or equal to    |         |
+| >        | greater than             |         |
+| <        | less than                |         |
+| !==      | not strictly equal       |         |
+| !=       | not loosely equal        |         |
+ */
+
 // Logical
+
+/**
+ * These operators help structure the logic of statements.
+| Operator | Action | Example |
+| -------- | ------ | ------- |
+| &&       | AND    |         |
+| OR       | OR     |         |
+ */
+
 // Unary
+
+/**
+ * These operators act on one piece of data and return something else
+| Operator | Action                           | Example |
+| -------- | -------------------------------- | ------- |
+| !        | NOT                              |         |
+| typeof   | returns the data type of operand |         |
+| -        | not sure yet                     |         |
+ */
+
 // Ternary
+
+/**
+ * a ? b : c
+This operator returns certain operands if first operand evaluates to true.
+
+Format: a ? b : c
+Understand as: "if a is true, return b, and if a is not true, return c"
+
+Examples:
+42 > 8 ? true : false  // true
+
+let string = "Holy Cow";
+string === "Holy Pig" ? "The pig is holy." : "The pig is not holy." // "The pig is not holy."  
+
+ */
