e8ad7c905c7b4d7dc984f5824aac086964f9af22
Mike Sammartino <sammartino.mike@gmail.com>
2024-01-23 01:40:19 +0000

    added basic structure

---
+++ studies/functions.js
@@ -0,0 +1,12 @@
+/**
+ * FUNCTIONS
+ * 
+ * 1. Defining and Invoking
+ * 2. Parameters and Arguments
+ * 3. Syntax for Named Functions
+ * 4. Assigning a Function to a Variable
+ * 5. How do we specify inputs and outputs?
+ * 6. Scope
+ * 7. Closures
+ * 
+ */
