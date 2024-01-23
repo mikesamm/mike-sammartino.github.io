cf03501982efa09527128496865dc4c401042aea
Mike Sammartino <sammartino.mike@gmail.com>
2024-01-23 01:32:26 +0000

    added rough draft and structure

---
+++ studies/string-manipulation.js
@@ -0,0 +1,30 @@
+/**
+ * STRING MANIPULATION
+ * 
+ * Strings are array-like. Strings are immutable, but reassignable. 
+ * We can interact with them with operators and string methods like .slice() and .join().
+ * Strings are iterable, although it may be useful to split a string into an array to then use array methods. 
+ * 
+ * 1. With Operators
+ * 
+ * 2. With String Methods
+ */
+
+/* With Operators
+
++, +=, more? is there a reason for more? 
+comparison operators
+*/
+
+/* With String Methods
+
+| Method         | Action                     
+| -------------- | -------------------------- 
+| .includes()    | returns true or false if argument is found in string
+| .replaceAll()  | replaces all instances of a character or regular expression
+| .slice()       | returns a new string, a substring from specified index range of original string         
+| .split()       | returns an array populated with chars from a string, takes in a string to determine where it separates original string
+| .toLowerCase() | returns new string, an all lowercase version of the original 
+| .toUpperCase() | returns new string, an all uppercase version of the original
+
+*/
